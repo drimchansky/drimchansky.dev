c8bf74a1ea13b6a1f36dfed995741541b00fa3b5
Nikita Chernov <drimchansky@gmail.com>
2024-04-28 18:35:12 +0400

    update localizations

---
+++ src/i18n/locales/ru.ts
@@ -0,0 +1,14 @@
+import type { LocaleCollection } from '..'
+
+export const ru: LocaleCollection = {
+  name: 'Никита Чернов',
+  siteDescription: 'Личный сайт Никиты Чернова',
+  homePage: 'Главная',
+  resume: 'Резюме',
+  theme: 'Тема',
+  light: 'Светлая',
+  auto: 'Авто',
+  dark: 'Темная',
+  toLocale: 'English',
+  404: 'Page not found'
+}
--- src/utils/functions.ts
+++
@@ -1,5 +0,0 @@
-import type { Locale } from './types'
-
-export const getLocale = (raw: string | undefined): Locale => {
-  return (raw || 'en') as Locale
-}
