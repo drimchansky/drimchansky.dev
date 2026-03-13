b492760d3de8a3321a46dc0295a87a18c6a466e7
Nikita Chernov <drimchansky@gmail.com>
2026-03-13 13:58:41 +0700

    Migrate from Vercel to Cloudflare Pages, remove Google Analytics/Partytown

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -18,7 +18,7 @@ const getRobotsTxt = (sitemapURL: URL, isProd: boolean) => {
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site)
-  const isProd = import.meta.env.VERCEL_ENV === 'production'
+  const isProd = import.meta.env.IS_PRODUCTION === '1'
 
   return new Response(getRobotsTxt(sitemapURL, isProd))
 }
